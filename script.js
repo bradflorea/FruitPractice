72e364deead8bc9c0d13a56967c93e8e4f16e00c
bradflorea <104443044+bradflorea@users.noreply.github.com>
2023-12-10 18:27:02 -0500

    Add files via upload
    Finished Project

---
+++ script.js
@@ -0,0 +1,48 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma','Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+// 3
+function search(str) {
+	let results = [];
+	results = fruit.filter(fruit => fruit.toLowerCase().includes(str.toLowerCase()));
+		
+	return results;
+	
+}
+
+//
+function searchHandler(e) {
+	showSuggestions(search(input.value));
+
+
+}
+
+//7
+function showSuggestions(results, inputVal) {
+	while (suggestions.firstChild) {
+		suggestions.removeChild(suggestions.firstChild);
+	}
+	for ( let result of results) {
+		let listItem = document.createElement('li');
+		listItem.innerHTML = result;
+		suggestions.appendChild(listItem);
+	}
+
+}
+
+// 6
+function useSuggestion(e) {
+	input.value = e.target.innerText;
+	while (suggestions.firstChild) {
+		suggestions.removeChild(suggestions.firstChild);
+	}};
+   
+
+// 1d
+input.addEventListener('input', searchHandler);
+
+
+// 5
+suggestions.addEventListener('click', useSuggestion);
